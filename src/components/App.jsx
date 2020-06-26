b989878db6ea81b411837753484d2e3f7b9a6fd2
75Jozef <64250704+75Jozef@users.noreply.github.com>
2020-06-26 14:59:42 +0200

    event_handling_input_attributes_used

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [nameEntered, setNameEntered] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function posli() {
+    setNameEntered(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Ahoj {nameEntered}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={posli}>Submit</button>
     </div>
   );
 }
